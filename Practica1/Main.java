4eab901688d7eadb9486b4c19dcc206b8b6c4718
RodrigoGV11 <54073587+RodrigoGV11@users.noreply.github.com>
2019-08-20 10:55:06 -0500

    Delete Main.java

--- Practica1/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main{
-public static void main (String []args){
-System.out.println("java 12.0.2 2019-07-16");
-System.out.println("Java(TM) SE Runtime Environment (build 12.0.2+10)");
-System.out.println("Java HotSpot(TM) 64-Bit Server VM (build 12.0.2+10, mixed mode, sharing)");
-}
-}
