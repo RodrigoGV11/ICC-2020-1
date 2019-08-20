1ee27574d87a7afa398c3838a0aadcb3c87d2827
RodrigoGV11 <54073587+RodrigoGV11@users.noreply.github.com>
2019-08-20 10:52:27 -0500

    Add files via upload

---
+++ practica01/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+public static void main (String []args){
+System.out.println("java 12.0.2 2019-07-16");
+System.out.println("Java(TM) SE Runtime Environment (build 12.0.2+10)");
+System.out.println("Java HotSpot(TM) 64-Bit Server VM (build 12.0.2+10, mixed mode, sharing)");
+}
+}
